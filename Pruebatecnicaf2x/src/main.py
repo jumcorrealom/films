51729f412ba5d16c49a56943bb6d38c1d1be341f
juanmacorreaDKT <juanmanuel.correa@decathlon.com>
2024-10-06 16:19:19 -0500

    parquets

--- Pruebatecnicaf2x/src/main.py
+++ Pruebatecnicaf2x/src/main.py
@@ -1,3 +1,4 @@
+
 from pyspark.sql import SparkSession
 from src.file_readers.file_reader_factory import FileReaderFactory
 from src.processing.films_transformer import FilmsTransformer
@@ -10,16 +11,28 @@ import sys
 
 python_executable = sys.executable
 
+
 # Set environment variables for PySpark
 os.environ['PYSPARK_PYTHON'] = python_executable
 os.environ['PYSPARK_DRIVER_PYTHON'] = python_executable
 
 def main():
+    # Create Spark session
     spark = SparkSession.builder \
         .appName("DataPipelineApp") \
         .getOrCreate()
     
-    file_path = "data/Films.xlsx"
+    # Get the directory of the current script (main.py)
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    
+    # The parent directory of src/ (i.e., pruebatecnicaf2x/)
+    project_root = os.path.dirname(current_dir)
+    
+    # Path to the data directory
+    data_dir = os.path.join(project_root, "data")
+    
+    # Input file path
+    file_path = os.path.join(data_dir, "Films.xlsx")
     file_type = file_path.split('.')[-1]
     
     # Get the appropriate reader
@@ -41,14 +54,21 @@ def main():
     for sheet_name, df in dfs.items():
         transformer = transformers.get(sheet_name)
         if transformer:
-            df = transformer.transform(df)
+            df = transformer.transform(df, source_file=file_path, sheet_name=sheet_name)
             dfs[sheet_name] = df
-            print(f"Transformed data for sheet '{sheet_name}':")
-            df.show()
+
+            # Define the output directory path
+            output_dir = os.path.join(data_dir, "processed_data", sheet_name)
+            os.makedirs(output_dir, exist_ok=True)
+            output_path = os.path.join(output_dir, f"{sheet_name}.parquet")
+
+            # Write to Parquet locally
+            transformer.write_to_parquet(df, output_path)
+            #transformer.write_to_csv(df, output_path)
+            
+            print(f"Data for sheet '{sheet_name}' has been written to {output_path}")
         else:
             print(f"No transformer for sheet '{sheet_name}', skipping.")
-    
-    # Optionally, write the transformed data to storage or proceed with further processing
     
     spark.stop()
 
